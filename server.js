2dd4d4a729042a2251c9adcf6c802ca7287da145
lex <savchenko.lex@gmail.com>
2023-11-17 09:50:08 +0200

    minor changes to start server code

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 const app = require("./app");
 const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose
   .connect(DB_HOST)
-  .then(() => app.listen(PORT, () => console.log("Database connection successful")))
+  .then(() =>  console.log("Database connection successful"))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
+
+  app.listen(PORT, () => {
+    console.log(`Server running using port: ${PORT}`);
+  })
