3323ffc05e9b6d41842df87702f5792a0a9c343b
admin <maksym.shunder95@gmail.com>
2023-11-19 20:52:30 +0200

    add notice endpoint

---
+++ controllers/notices/addNotice.js
@@ -0,0 +1,26 @@
+const Notice = require('../../models/notice')
+
+const addNotice = async (req, res, next) => {
+    const {id: owner, email, phone} = req.user;
+
+    const {name, title, birthday, type, comments, category, location, sex} =
+        req.body;
+
+    const resolve = await Notice.create({
+        name,
+        birthday,
+        type,
+        category,
+        comments,
+        owner,
+        sex,
+        phone,
+        email,
+        location,
+        title,
+    });
+
+    res.status(201).json(resolve);
+};
+
+module.exports = addNotice;
