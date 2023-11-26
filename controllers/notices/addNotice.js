6316368089838d15f223ce0b4d358a0fd0697458
admin <maksym.shunder95@gmail.com>
2023-11-26 15:16:43 +0200

    adding validation to add notice

--- controllers/notices/addNotice.js
+++ controllers/notices/addNotice.js
@@ -1,4 +1,6 @@
-const Notice = require('../../models/notice')
+const Notice = require('../../models/notice');
+const noticeSchema = require('../../schemas/noticeSchema')
+const requestError = require("../../helpers/requestError");
 
 const addNotice = async (req, res, next) => {
     const {id: owner, email, phone} = req.user;
@@ -6,6 +8,22 @@ const addNotice = async (req, res, next) => {
 
     const {name, title, birthday, type, comments, category, location, sex, price} =
         req.body;
+
+    const {error} = noticeSchema.validate({
+        name,
+        title,
+        birthday,
+        type,
+        comments,
+        category,
+        location,
+        sex,
+        price,
+        owner,
+        phone,
+        email
+    })
+    if (error) throw requestError(400, error);
 
     const resolve = await Notice.create({
         title,
@@ -21,6 +39,11 @@ const addNotice = async (req, res, next) => {
         phone,
         email,
         owner
+    }, {
+        createdAt: 0,
+        updatedAt: 0,
+        owner: 0,
+        _id: 0
     });
 
     res.status(201).json(resolve);
