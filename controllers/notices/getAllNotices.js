c6248f8617e9949e24261e6b77bc1a918b26008a
lex <savchenko.lex@gmail.com>
2023-11-28 05:42:14 +0200

    add pagination to get notices

--- controllers/notices/getAllNotices.js
+++ controllers/notices/getAllNotices.js
@@ -2,10 +2,24 @@ const Notices = require("../../models/notice");
 
 
 const getAllNotices = async (req, res, next) => {
+  const offset = parseInt(req.query.offset) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+  
   try {
-    const resolve = await Notices.find();
+    const total = await Notices.countDocuments();
+    const totalPages = Math.ceil(total / limit);
+    const resourses = await Notices.find()
+                          .skip((offset - 1) * limit)
+                          .limit(limit)
+                          .exec();
 
-    return res.status(201).json(resolve);
+    return res.status(200).json({status: "ok",
+                                 code: 200,
+                                 data:{
+                                  resourses,
+                                  total,
+                                  totalPages
+                                 }});
   } catch (error) {
     next(error);
   }
