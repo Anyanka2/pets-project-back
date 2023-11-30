188117e1d459c3bba41e0054fbe5c18f8473eb60
Liu3oIT <liutyivova995@gmail.com>
2023-11-30 22:03:03 +0200

    update

--- models/notice.js
+++ models/notice.js
@@ -1,58 +1,58 @@
-const {Schema, model} = require("mongoose");
+const { Schema, model } = require("mongoose");
 
 const noticeSchema = Schema(
-    {
-        category: {
-            type: String,
-            required: [true, "Category is required"],
-        },
-        title: {
-            type: String,
-            required: [true, "Notices title is required"],
-        },
-        name: {
-            type: String,
-            required: [true, "Pet name is required"],
-        },
-        birthday: {
-            type: Date,
-        },
-        type: {
-            type: String,
-        },
-        location: {
-            type: String,
-        },
-        sex: {
-            type: String,
-            required: [true, "Sex is required"],
-        },
-        price: {
-            type: Number,
-        },
-        phone: {
-            type: String,
-        },
-        email: {
-            type: String,
-            required: [true, "Email is required"],
-        },
-        comments: {
-            type: String,
-            required: [true, "Comments is required"],
-        },
-        imageUrl: {
-            type: String,
-        },
-        owner: {
-            type: String,
-            required: [true, "Owner is required"],
-        },
+  {
+    category: {
+      type: String,
+      required: [true, "Category is required"],
     },
-    {
-        timestamps: true,
-        versionKey: false,
-    }
+    title: {
+      type: String,
+      required: [true, "Notices title is required"],
+    },
+    name: {
+      type: String,
+      required: [true, "Pet name is required"],
+    },
+    birthday: {
+      type: String,
+    },
+    type: {
+      type: String,
+    },
+    location: {
+      type: String,
+    },
+    sex: {
+      type: String,
+      required: [true, "Sex is required"],
+    },
+    price: {
+      type: Number,
+    },
+    phone: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+    },
+    comments: {
+      type: String,
+      required: [true, "Comments is required"],
+    },
+    imageUrl: {
+      type: String,
+    },
+    owner: {
+      type: String,
+      required: [true, "Owner is required"],
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
 );
 
 const Notice = model("Notice", noticeSchema);
