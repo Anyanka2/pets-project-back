e1aeff9cbdc3e918fbabbc0c791707d911d514b3
lex <savchenko.lex@gmail.com>
2024-01-14 16:23:23 +0200

    add auth middleware and get req.body

--- controllers/news/addNews.controller.js
+++ controllers/news/addNews.controller.js
@@ -2,10 +2,10 @@ const NewsModel = require('../../models/news.model');
 const newsJoiSchema = require('../../schemas/newsJoiSchema');
 
 async function addNews (req, res, next) {
+    const {body, user} = req;
+    console.log(body);
 
-    console.log(req);
-
-    res.status(200).json({"message": "ok"})
+    res.status(200).json({"message": "ok"});
 }
 
 module.exports = addNews;
--- routes/api/news.router.js
+++ routes/api/news.router.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const controller = require('../../controllers/news');
 const controllerWrapper = require("../../helpers/controllerWrappers");
+const auth = require('../../middlewares/authMiddleware');
 
 router.get('/', controllerWrapper(controller.getNews));
-router.post('/', controllerWrapper(controller.addNews));
+router.post('/',controllerWrapper(auth), controllerWrapper(controller.addNews));
 
 module.exports = router;
