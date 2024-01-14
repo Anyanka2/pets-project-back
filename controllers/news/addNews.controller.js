33b180ebb1693398bfdf7e91e8f68c3ff5bb445b
lex <savchenko.lex@gmail.com>
2024-01-14 16:04:02 +0200

    create simple joi schema

--- controllers/news/addNews.controller.js
+++ controllers/news/addNews.controller.js
@@ -1,7 +1,9 @@
 const NewsModel = require('../../models/news.model');
-
+const newsJoiSchema = require('../../schemas/newsJoiSchema');
 
 async function addNews (req, res, next) {
+
+    console.log(req);
 
     res.status(200).json({"message": "ok"})
 }
