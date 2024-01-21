7e3cd6338af3bde3a64d11b46b33e121ba959297
lex <savchenko.lex@gmail.com>
2024-01-21 03:39:45 +0200

    looks like finish work with add news route

--- controllers/news/addNews.controller.js
+++ controllers/news/addNews.controller.js
@@ -3,16 +3,22 @@ const newsJoiSchema = require('../../schemas/newsJoiSchema');
 const requestError = require('../../helpers/requestError');
 
 async function addNews (req, res, next) {
-    const {body, user} = req;
-    
+    const {body, user} = req;   
     const level = user.permissionLevel;
 
     if (level === "admin" || level === "manager" ) {
         
-        console.log(user);
-        res.status(200).json({"message": body});
+        const {error, value} = newsJoiSchema.validate(body);
+
+        if (error) { 
+            next(requestError(400, error.message));
+            //res.status(400).json(error.message);
+        } else {
+            NewsModel.create(value);
+            res.status(200).json({"message": value});
+        }
     }
-    requestError(401);
+    next(requestError(401));
 }
 
 module.exports = addNews;
--- schemas/newsJoiSchema.js
+++ schemas/newsJoiSchema.js
@@ -1,11 +1,11 @@
 const joi = require("joi");
 
 const newsJoiSchema = joi.object({
-    imgUrl: joi.string().optional().allow(null),
+    imgUrl: joi.string().required(),
     title: joi.string().required(),
     text: joi.string().required(),
-    date: joi.string().required(),
-    url: joi.string().optional().allow(null),
+    date: joi.date().required(),
+    url: joi.string().required(),
     nytID: joi.string().optional().allow(null),
 })
 
